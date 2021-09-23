f25817ad357b1da6dc270e133b15671d0197e6d1
flagofflak <86945106+offlak@users.noreply.github.com>
2021-09-23 14:25:41 +0900

    アニメーション追加

---
+++ js/8-17.js
@@ -0,0 +1,52 @@
+function GlowAnimeControl() {
+	$('.glowAnime').each(function () {
+		var elemPos = $(this).offset().top - 50;
+		var scroll = $(window).scrollTop();
+		var windowHeight = $(window).height();
+		if (scroll >= elemPos - windowHeight) {
+			$(this).addClass("glow");
+
+		} else {
+			$(this).removeClass("glow");
+		}
+	});
+}
+function EachTextAnimeControl() {
+  $('.eachTextAnime').each(function () {
+    var elemPos = $(this).offset().top - 50;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight) {
+      $(this).addClass("appeartext");
+
+    } else {
+      $(this).removeClass("appeartext");
+    }
+  });
+}
+$(window).on('load', function () {
+	//spanタグを追加する
+	var element = $(".glowAnime");
+	element.each(function () {
+		var text = $(this).text();
+		var textbox = "";
+		text.split('').forEach(function (t, i) {
+			if (t !== " ") {
+				if (i < 10) {
+					textbox += '<span style="animation-delay: .' + i + 's;">' + t + '</span>';
+				} else {
+					var n = i / 10;
+					textbox += '<span style="animation-delay: ' + n + 's;">' + t + '</span>';
+				}
+
+			} else {
+				textbox += t;
+			}
+		});
+		$(this).html(textbox);
+	});
+	GlowAnimeControl();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+$(window).scroll(function () {
+  EachTextAnimeControl();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面をスクロールをしたら動かしたい場合の記述
